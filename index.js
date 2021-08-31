9b4485e88c9ba41a5d17b0f32a55374f177ba46c
mrbjn99 <mrbjn99@gmail.com>
2021-08-31 20:41:11 +0700

    add find 1 item

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const app = express();
 const importData = require("./data.json");
-let port = process.env.PORT || 5000;
 
+let port = process.env.PORT || 5000;
 
 app.get("/", (req , res) =>{
     res.send("Welcome to Real-estate-api")
@@ -12,6 +12,15 @@ app.get("/items", (req,res) => {
 
     res.send(importData);
 });
+
+app.get("/items/:ID", (req, res) => {
+    const { ID } = req.params;
+    
+    const foundItem = importData.find((item) => item.ID == ID);
+    
+    res.send(foundItem);
+
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
