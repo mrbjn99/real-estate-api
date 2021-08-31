a58ab9b082affc91f85ee4bcd536c3f911d1e297
mrbjn99 <mrbjn99@gmail.com>
2021-08-31 17:41:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =require("express");
+const app = express();
+const importData = require("../real-estate-api/data.json");
+let port = process.env.PORT || 5000;
+
+
+app.get("/", (req , res) =>{
+    res.send("Welcome to Real-estate-api")
+});
+
+app.get("/items", (req,res) => {
+
+    res.send(importData);
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
